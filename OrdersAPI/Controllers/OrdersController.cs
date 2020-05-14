83052cb379779d8a29d9362a0541fd5a382bcb6b
KDSKardabox <bollygame94@gmail.com>
2020-05-14 16:11:21 +0530

    Initial orders API repository

---
+++ OrdersAPI/Controllers/OrdersController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrdersAPI.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class OrdersController : ControllerBase
+  {
+    [HttpGet("{id}")]
+    public IEnumerable<Order> Get(int id)
+    {
+      return new List<Order>() {
+        new Order() { OrderId = 1, OrderAmount = 250, OrderData = DateTime.Now.AddDays(-2) },
+        new Order() { OrderId = 2, OrderAmount = 450, OrderData = DateTime.Now.AddDays(-1) }
+      };
+
+    }
+  }
+}
---
+++ OrdersAPI/Models/Order.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Order
+{
+  public int OrderId { get; set; }
+
+  public int OrderAmount { get; set; }
+
+  public DateTime OrderData { get; set; }
+}
