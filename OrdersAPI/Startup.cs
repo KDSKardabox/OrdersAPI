6efa67ce9587d87e83410882b0dec17134421b82
KDSKardabox <bollygame94@gmail.com>
2020-05-14 16:23:28 +0530

    Add Jaeger Tracing

--- OrdersAPI/Startup.cs
+++ OrdersAPI/Startup.cs
@@ -1,10 +1,14 @@
-
+using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Jaeger;
+using Jaeger.Samplers;
+using OpenTracing;
+using OpenTracing.Util;
 
 namespace OrdersAPI
 {
@@ -20,6 +24,19 @@ namespace OrdersAPI
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
+      services.AddSingleton<ITracer>(serviceProvider =>
+        {
+          string serviceName = Assembly.GetEntryAssembly().GetName().Name;
+          ILoggerFactory loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+          ISampler sampler = new ConstSampler(sample: true);
+          ITracer tracer = new Tracer.Builder(serviceName)
+              .WithLoggerFactory(loggerFactory)
+              .WithSampler(sampler)
+              .Build();
+          GlobalTracer.Register(tracer);
+          return tracer;
+        });
+      services.AddOpenTracing();
       services.AddControllers();
     }
 
